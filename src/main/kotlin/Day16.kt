8bc3e2334566689e63bbd5e11e9307d28e2d5837
Zordid <Zordid@users.noreply.github.com>
2019-12-17 08:31:24 +0100

    Clean up day 16 mess

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -2,21 +2,12 @@ import kotlin.math.absoluteValue
 
 class Day16(testData: List<String>? = null) : Day<String>(16, 2019, ::asStrings, testData) {
 
-    val signal = input[0].toCharArray().map { it.toString().toInt() }
-
     val basePattern = sequenceOf(0, 1, 0, -1)
 
-    fun patternForDigit(n: Int) = basePattern.asRepeatingSequence(n).asInfiniteSequence().drop(1)
+    fun String.toSignal() = toCharArray().map { it.toString().toInt() }.toIntArray()
+    fun IntArray.toString() = joinToString("")
 
-    fun fft(input: List<Int>): List<Int> {
-        val output = mutableListOf<Int>()
-        for (idx in input.indices) {
-            val pattern = patternForDigit(idx + 1)
-            //println("$idx: ${pattern.take(input.size).joinToString()}")
-            output.add(input.asSequence().zip(pattern).map { (i, p) -> i * p }.sum().absoluteValue % 10)
-        }
-        return output
-    }
+    fun patternForDigit(n: Int) = basePattern.asRepeatingSequence(n).asInfiniteSequence().drop(1)
 
     fun <T> Sequence<T>.asRepeatingSequence(f: Int): Sequence<T> = sequence {
         for (element in this@asRepeatingSequence) {
@@ -28,112 +19,38 @@ class Day16(testData: List<String>? = null) : Day<String>(16, 2019, ::asStrings,
         while (true) yieldAll(this@asInfiniteSequence)
     }
 
+    fun fft(input: IntArray): IntArray {
+        val output = input.copyOf()
+        for (idx in input.indices) {
+            val pattern = patternForDigit(idx + 1)
+            output[idx] = input.asSequence().zip(pattern).map { (i, p) -> i * p }.sum().absoluteValue % 10
+        }
+        return output
+    }
+
     override fun part1(): String {
-        //var s = signal
-        var s = input[0].repeat(signalRepeat).toCharArray().map { it.toString().toInt() }
-        println("Input signal: ${s.joinToString("")}")
-        repeat(phasesMAX) {
+        var s = input[0].toSignal()
+        //println("Input signal: ${s.joinToString("")}")
+        repeat(100) {
             s = fft(s)
-            println("After ${it + 1} phase: ${s.joinToString("")}")
+            //println("After ${it + 1} phase: ${s}")
         }
-        return s.slice(offset + 0..offset + 7).joinToString("")
+        return s.slice(0..7).joinToString("")
     }
 
-    val cache = mutableMapOf<Int, Int>()
-
-    fun periodOfPhase(phase: Int): Int {
-        if (phase == 0)
-            return lcm(4, signal.size)
-        return lcm(4, periodOfPhase(phase - 1))
-    }
-
-    fun digit(n: Int, phase: Int): Int {
-        if (phase == 0) {
-            return signal[(n - 1) % signal.size]
-        }
-//        val cacheKey = n * 1000 + phase
-//        if (cache.containsKey(cacheKey))
-//            return cache[cacheKey]!!
-
-        //val patternForDigit = patternForDigit(n).iterator()
-
-        var sum = 0
-        var idx = n
-        var f = 1
-        var fCount = n
-
-        // start at n repeat pattern block is n values
-        val period = 4 * n
-
-        val periodLength = lcm(period, signal.size)
-
-        val fullPeriods = signal.size * signalRepeat / periodLength
-        val leftOver = signal.size * signalRepeat % periodLength
-
-        // 1 pattern
-        if (fullPeriods > 0) {
-            while (idx <= periodLength) {
-                val d = digit(idx, phase - 1)
-                sum += (f * d)
-                idx++
-                fCount--
-                if (fCount == 0) {
-                    idx += n
-                    f = -f
-                    fCount = n
-                }
-
-            }
-            require(fCount == n)
-            require(f == 1)
-            sum *= fullPeriods
-        }
-        if (leftOver > 0) {
-            while (idx <= signal.size * signalRepeat) {
-                val d = digit(idx, phase - 1)
-                sum += (f * d) % 10
-                idx++
-                fCount--
-                if (fCount == 0) {
-                    idx += n
-                    f = -f
-                    fCount = n
-                }
+    override fun part2(): String {
+        val offset = input[0].slice(0..6).toInt()
+        val s = input[0].repeat(10000).toSignal()
+        repeat(100) {
+            for (pos in s.lastIndex - 1 downTo offset) {
+                s[pos] = (s[pos] + s[pos + 1]) % 10
             }
         }
-        val result = sum.absoluteValue % 10
-        //cache[cacheKey] = result
-        return result
+        return s.slice(offset..offset + 7).joinToString("")
     }
-
-    val factors = listOf(0, 1, 0, -1)
-
-    fun factorFor(idx: Int, n: Int): Int {
-        val bucket = idx / n
-        return factors[bucket % 4]
-    }
-
-    val signalRepeat = 100
-    val phasesMAX = 2
-    var offset = 0// signal.slice(0..6).joinToString("").toInt()
-
-    override fun part2(): Any? {
-        for (i in (offset + 1..offset + 24)) {
-            print(digit(i, 1))
-        }
-        println()
-        for (i in (offset + 1..offset + 24)) {
-            print(digit(i, phasesMAX))
-        }
-        println()
-
-        return ""
-    }
-
 
 }
 
 fun main() {
-    val testData = listOf("12345678")
-    Day16(testData).run()
+    Day16().run()
 }
---
+++ src/test/kotlin/Day16Test.kt
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class Day16Test {
+
+    @Test
+    fun `part 1`() {
+        assertEquals("58672132", Day16().part1())
+    }
+
+    @Test
+    fun `part 2`() {
+        assertEquals("91689380", Day16().part2())
+    }
+
+}
